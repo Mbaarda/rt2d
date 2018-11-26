41654a5e456d985e67f47179aa8a589cc567ed2a
Marijn Baarda <Marijn Baarda>
2018-11-26 09:27:15 +0100

    board toegevoegd

---
+++ checkers/myentity.cpp
@@ -0,0 +1,22 @@
+/**
+ * This class describes MyEntity behavior.
+ *
+ * Copyright 2015 Your Name <you@yourhost.com>
+ */
+
+#include "myentity.h"
+
+MyEntity::MyEntity() : Entity()
+{
+	this->addSprite("assets/board.tga");
+}
+
+MyEntity::~MyEntity()
+{
+
+}
+
+void MyEntity::update(float deltaTime)
+{
+
+}
